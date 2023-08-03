204431e59776736dba07ac7d4590357ce14067a3
Morgan Bergen <morgan@ku.edu>
2023-08-02 21:00:19 -0400

    added dependencies

--- src/llm-app/chat.py
+++ src/llm-app/chat.py
@@ -1,32 +1,38 @@
-"""   
+"""
 @package chat.py
 @description    query with gpt-3 and return response
-             
+
                 <ACTION> someone messages the bot in a discord channel
                 ->
                 msg.content
                 ->
-                const response = query_openai(msg.content) 
+                const response = query_openai(msg.content)
                 ->
                 msg.reply(response)
                 ->
                 <ACTION> a reply happens in the discord channel
-"""   
+
+@dependencies   pip install openai
+                pip install chromadb
+                pip install tiktoken
+
+"""
 
 import os
 import sys
 import constants
 from langchain.document_loaders import TextLoader
-from langchain.indexes import VectorestroreIndexCreator
+from langchain.indexes import VectorstoreIndexCreator
 
 os.environ["OPENAI_API_KEY"] = constants.APIKEY
 
-query sys.argv[1]
+# Get the question from the command-line argument
+query = sys.argv[1]
 
 print(query)
 
-loader = TextLoader('data.txt');
+loader = TextLoader('data.txt')
+index = VectorstoreIndexCreator().from_loaders([loader])
 
-index = VectorestoreIndexCreator().from_loaders((loader))
+print(index.query(query))
 
-print (index.query(query))
