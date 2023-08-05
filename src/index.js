8941761d4a96896e38e42957369746f0e7495db2
Emir Avci <emiravc@gmail.com>
2023-08-05 15:47:06 +0300

    ClaimNFT query return 🚀

--- src/index.js
+++ src/index.js
@@ -1,58 +1,64 @@
 // index.js
+// Import required packages and modules
 require('dotenv').config();
-const { Client, IntentsBitField } = require('discord.js');
+const { Client, IntentsBitField, MessageAttachment } = require('discord.js'); // Import MessageAttachment for sending images
 const { spawn } = require('child_process');
 
+// Initialize a new Discord client
 const client = new Client({
-    // intents is set of permissions bot can use
-    // more info on discord develop intent site
     intents : [
       IntentsBitField.Flags.Guilds,
       IntentsBitField.Flags.GuildMembers,
       IntentsBitField.Flags.GuildMessages,
       IntentsBitField.Flags.MessageContent,
     ],
-  });
+});
 
-// When the bot is ready
+// Event to log once the bot is ready
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
 });
 
-// When a message is received
+// Event for when a message is created in the server
 client.on('messageCreate', async (msg) => {
-    // Ignore messages from bots
+    // If the message is from a bot, ignore it
     if (msg.author.bot) {
         return;
     }
 
-    // Check if the message is a command for the Python script (e.g., '!ask')
-    if (msg.content.startsWith('!ask')) {
-        // Extract the question from the message content
-        const question = msg.content.slice(5).trim();
+    // Check if the message is "Claim NFT" (case insensitive)
+    // If so, attach the "ClaimNFT.png" image from the assets directory and send it
+    // Then return to prevent executing any further code in this callback
+    if (msg.content.toLowerCase() === 'claim nft') {
+        const image = new MessageAttachment('./assets/ClaimNFT.png');
+        msg.channel.send({ files: [image] }); 
+        return;
+    }
 
-        // log the question on cmd
-        console.log(question);
-        
-        // to joseph / emir, just ask specific question and it'll trigger what you want
+    // Check if the message starts with "!ask" command
+    if (msg.content.startsWith('!ask')) {
+        const question = msg.content.slice(5).trim(); 
+        console.log(question); 
+
+        // If the message is "ping", reply with "pong"
         if (msg.content === 'ping') {
             msg.reply('pong');
         }
 
-        // Create a Python child process and pass the question as a command-line argument
+        // Spawn a Python child process with the question as an argument
         const pythonProcess = spawn('python3', ['chat.py', question]);
-
         let response = '';
 
-        // Capture the output of the Python script
+        // Listen to the "data" event of the python process's stdout stream and accumulate the data
         pythonProcess.stdout.on('data', (data) => {
             response += data.toString();
         });
-        
-        // Wait for the Python script to finish
+
+        // Listen to the "close" event of the python process
         pythonProcess.on('close', (code) => {
+            // If the process ended successfully, reply with the accumulated response
+            // Otherwise, log the exit code to the console
             if (code === 0) {
-                // If the Python script ran successfully, send the response back to the Discord channel
                 msg.reply(response);
             } else {
                 console.error(`Python script exited with code ${code}`);
@@ -61,6 +67,5 @@ client.on('messageCreate', async (msg) => {
     }
 });
 
-// Log in to Discord with the bot token
+// Log in to the Discord server using the token from the environment variables
 client.login(process.env.TOKEN);
-
